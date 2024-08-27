433b226b0f5ffdff1ba848625672492d4ccd02dd
Dewi <dewisartika120729@gmail.com>
2024-08-27 11:00:02 +0700

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,36 @@
+// routes.js
+const {
+  addbooksHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addbooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler:  deleteBookByIdHandler,
+  },
+];
+module.exports = routes;
